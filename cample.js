ba36de4e376e3ed6a4d280e5b0b77362dab5e43e
Takeshi Ueda <uedatakeshi@gmail.com>
2015-12-26 17:48:30 +0900

    add preset 2015

--- cample.js
+++ cample.js
@@ -1,18 +1,31 @@
 "use strict";
 
-var plus = (a, b) => a + b;
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var plus = function plus(a, b) {
+    return a + b;
+};
 
 console.log(plus(1, 2));
 
-class Person {
-    constructor(name) {
+var Person = (function () {
+    function Person(name) {
+        _classCallCheck(this, Person);
+
         this.name = name;
     }
 
-    greet() {
-        console.log("Hello, I'm " + this.name);
-    }
-}
+    _createClass(Person, [{
+        key: "greet",
+        value: function greet() {
+            console.log("Hello, I'm " + this.name);
+        }
+    }]);
+
+    return Person;
+})();
 
 var bob = new Person("Bob");
 bob.greet();
